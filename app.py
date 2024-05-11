92a0561e4921c0fab80e9b9dc55ffb120c0f8e8f
Manikandan K <140843246+MANIKANDAN1201@users.noreply.github.com>
2024-05-11 19:08:32 +0000

    modified frontend

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/index")
 def index():
   return render_template('index.html')
+
+
+@app.route("/home")
+def home():
+  return render_template('home.html')
+
+
+@app.route("/single")
+def single():
+  return render_template('single.html')
 
 
 if __name__ == '__main__':
